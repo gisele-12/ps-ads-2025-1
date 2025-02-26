9c84515e36ef3ea810bb2ab08b90ff103d695b8b
Fausto G. Cintra <professor@faustocintra.com.br>
2025-02-26 10:20:33 -0300

    (26/02) Término do CRUD do model Customers

--- back-end/src/controllers/customers.js
+++ back-end/src/controllers/customers.js
@@ -47,4 +47,69 @@ controller.retrieveAll = async function (req, res) {
   }
 }
 
+controller.retrieveOne = async function (req, res) {
+  try {
+    // Busca no banco de dados apenas o cliente indicado
+    // pelo parâmetro "id"
+    const result = await prisma.customer.findUnique({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Encontrou ~> HTTP 200: OK (implícito)
+    if(result) res.send(result)
+
+    // Não encontrou ~> HTTP 404: Not found
+    else res.status(404).end()
+  }
+  catch(error) {
+    // Se algo de errado acontecer, cairemos aqui
+    // Nesse caso, vamos exibir o erro no console e enviar
+    // o código HTTP correspondente a erro do servidor
+    // HTTP 500: Internal Server Error
+    console.error(error)
+    res.status(500).end()
+  }
+}
+
+controller.update = async function(req, res) {
+  try {
+    // Busca o registro no banco de dados pelo seu id
+    // e atualiza as informações com o conteúdo de req.body
+    await prisma.customer.update({
+      where: { id: Number(req.params.id) },
+      data: req.body
+    })
+
+    // Encontrou e atualizou ~> HTTP 204: No content
+    res.status(204).end()
+  }
+  catch(error) {
+    console.error(error)
+    
+    // Não encontrou e não atualizou ~> HTTP 404: Not found
+    if(error?.code === 'P2025') res.status(404).end()
+    // Outros tipos de erro ~> HTTP 500: Internal server error
+    else res.status(500).end()
+  }
+}
+
+controller.delete = async function(req, res) {
+  try {
+    await prisma.customer.delete({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Encontrou e excluiu ~> HTTP 204: No content
+    res.status(204).end()
+  }
+  catch(error) {
+    console.error(error)
+    
+    // Não encontrou e não excluiu ~> HTTP 404: Not found
+    if(error?.code === 'P2025') res.status(404).end()
+    // Outros tipos de erro ~> HTTP 500: Internal server error
+    else res.status(500).end()
+  }
+}
+
 export default controller
