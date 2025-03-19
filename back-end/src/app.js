fb591a45c69c4fc075de2a15635ffeabfb35246a
Fausto G. Cintra <professor@faustocintra.com.br>
2025-03-19 10:55:32 -0300

    (19/03) Início da integração com front-end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,23 @@
+// Carregando as variáveis de ambiente do arquivo .env
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
 import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
 
 const app = express()
+
+// Configurando o CORS para aceitar requisições a partir
+// dos servidores configurados na variável de ambiente
+// ALLOWED_ORIGINS
+import cors from 'cors'
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS.split(','),
+  // credentials: true
+}))
 
 app.use(logger('dev'))
 app.use(json())
@@ -13,7 +25,6 @@ app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 //------------------- ROTAS ---------------------------
 
@@ -22,5 +33,8 @@ app.use('/customers', customersRouter)
 
 import carsRouter from './routes/cars.js'
 app.use('/cars', carsRouter)
+
+import usersRouter from './routes/users.js'
+app.use('/users', usersRouter)
 
 export default app
