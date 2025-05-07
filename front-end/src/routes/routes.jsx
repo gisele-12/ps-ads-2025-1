25c66082985955ca47a6377c0106791540ed057b
Gisele ROncoleta <gisele.roncoleta@fatec.sp.gov.br>
2025-05-07 10:56:17 -0300

    (07/05) Implementação do cadastro de usuários

--- front-end/src/routes/routes.jsx
+++ front-end/src/routes/routes.jsx
@@ -13,6 +13,7 @@ import CarsList from '../pages/cars/CarsList'
 import CarsForm from '../pages/cars/CarsForm'
 
 import UsersList from '../pages/users/UsersList'
+import UsersForm from '../pages/users/UsersForm'
 
 const routes = [
   {
@@ -74,7 +75,20 @@ const routes = [
     description: 'Listagem de usuários',
     element: <UsersList />,
     userLevel: 2
-  }
+  },
+  {
+    route: '/users/new',
+    description: 'Cadastro de usuários',
+    element: <UsersForm />,
+    userLevel: 2
+  },
+  {
+    route: '/users/:id',
+    description: 'Alterar usuário',
+    element: <UsersForm />,
+    userLevel: 2,
+    omitFromMainMenu: true
+  },
 ]
 
 export default routes
