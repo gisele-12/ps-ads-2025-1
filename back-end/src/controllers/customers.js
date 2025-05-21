5a9f04e931d3f2f9a9b6cbc4b079c3d06102a9a3
Fausto Cintra <professor@faustocintra.com.br>
2025-05-21 10:06:17 -0300

    (21/05) Término da implementação da validação de clientes no back-end

--- back-end/src/controllers/customers.js
+++ back-end/src/controllers/customers.js
@@ -1,9 +1,22 @@
 import prisma from '../database/client.js'
+import Customer from '../models/Customer.js'
+import { ZodError } from 'zod'
 
 const controller = {}   // Objeto vazio
 
 controller.create = async function (req, res) {
   try {
+
+    // Garante que a data de nascimento esteja no formato correto
+    // antes da validação
+    if(req.body.birth_date) req.body.birth_date = new Date(req.body.birth_date)
+
+    // Executa a validação do modelo do Zod para os
+    // dados que vieram em req.body
+    Customer.parse(req.body)
+
+    console.log('DADOS:', req.body)
+
     // Dentro do parâmetro req (requisição), haverá
     // um objeto chamado "body" que contém as informações
     // que queremos armazenar do BD. Então, invocamos o
@@ -18,11 +31,14 @@ controller.create = async function (req, res) {
   }
   catch(error) {
     // Se algo de errado acontecer, cairemos aqui
-    // Nesse caso, vamos exibir o erro no console e enviar
-    // o código HTTP correspondente a erro do servidor
-    // HTTP 500: Internal Server Error
     console.error(error)
-    res.status(500).end()
+
+    // Se for erro de validação do Zod, retorna
+    // HTTP 422: Unprocessable entity
+    if(error instanceof ZodError) res.status(422).send(error.issues)
+
+    // Senão, retorna o habitual HTTP 500: Internal Server Error
+    else res.status(500).end()
   }
 }
 
@@ -73,6 +89,15 @@ controller.retrieveOne = async function (req, res) {
 
 controller.update = async function(req, res) {
   try {
+
+    // Garante que a data de nascimento esteja no formato correto
+    // antes da validação
+    if(req.body.birth_date) req.body.birth_date = new Date(req.body.birth_date)
+      
+    // Executa a validação do modelo do Zod para os
+    // dados que vieram em req.body
+    Customer.parse(req.body)
+
     // Busca o registro no banco de dados pelo seu id
     // e atualiza as informações com o conteúdo de req.body
     await prisma.customer.update({
@@ -84,12 +109,20 @@ controller.update = async function(req, res) {
     res.status(204).end()
   }
   catch(error) {
+    // Se algo de errado acontecer, cairemos aqui
     console.error(error)
-    
+
     // Não encontrou e não atualizou ~> HTTP 404: Not found
     if(error?.code === 'P2025') res.status(404).end()
-    // Outros tipos de erro ~> HTTP 500: Internal server error
+
+    // Se for erro de validação do Zod, retorna
+    // HTTP 422: Unprocessable entity
+    else if(error instanceof ZodError) res.status(422).send(error.issues)
+
+    // Senão, retorna o habitual HTTP 500: Internal Server Error
     else res.status(500).end()
+
+    console.error(error)
   }
 }
 
